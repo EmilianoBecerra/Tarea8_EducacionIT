508e82e6e780782606ab8e5a0088425e65a81e64
EmilianoBecerra <101412014+EmilianoBecerra@users.noreply.github.com>
2023-01-13 00:03:01 -0300

    Finalizado

--- index.js
+++ index.js
@@ -5,5 +5,37 @@ const cartelDatos = document.querySelector("#uno")
 const cartelContacto = document.querySelector("#dos")
 const cartelActividades = document.querySelector("#tres")
 const botonCambio = document.querySelector("#cambiarEstilos")
-const sinCss = document.querySelector("#style")
+const sinCss = document.querySelector("#style") 
 
+
+if(css.getAttribute("href") == 'estilos/estilos.css'){
+    sinCss.innerHTML = ""
+}
+
+botonCambio.addEventListener("click", () => {
+     if(css.getAttribute("href") == 'estilos/estilos.css'){
+        css.setAttribute("href", "estilos/estilos-retro.css")
+        sinCss.innerHTML = ""
+        cartelTitulo.innerHTML= "VENI AL GYM!!!"
+        cartelDatos.innerHTML= "Datos Personales"
+        cartelContacto.innerHTML= "Datos de Contacto"
+        cartelActividades.innerHTML= "Actividades"
+    }
+
+    else if(css.getAttribute("href") == 'estilos/estilos-retro.css'){
+        css.setAttribute("href", "estilos/estilos-futuro.css")
+        sinCss.innerHTML = ""
+        titulo.innerHTML = `<span style ="--l:'G';">G</span> 
+                            <span style ="--l:'Y';">Y</span> 
+                            <span style ="--l:'M';">M</span>`
+    }
+    
+    else if(css.getAttribute("href") == 'estilos/estilos-futuro.css'){
+        css.setAttribute("href", "estilos/estilos.css")
+        sinCss.innerHTML = ""
+        titulo.innerHTML = "Vení al GYM!"
+        cartelDatos.innerHTML= "🙋‍♂️ Datos Personales 👀"
+        cartelContacto.innerHTML= "📱Datos de Contacto ✉️"
+        cartelActividades.innerHTML= "🚴 Actividades 🤸"
+    }
+})
